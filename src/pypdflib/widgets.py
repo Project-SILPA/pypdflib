fec8c8974f51cb8a543e1a660a1ded94844f571f
Jishnu Mohan <jishnu7@gmail.com>
2013-10-09 22:42:07 +0530

    remove unwanted if conditions

--- src/pypdflib/widgets.py
+++ src/pypdflib/widgets.py
@@ -79,28 +79,20 @@ class Widget(object):
         
 class Paragraph(Widget):
     
-    def __init__(self,  text = None, markup = None, font = None, text_align = None, font_size = None):
+    def __init__(self,  text = None, markup = None, font = "Sans", text_align = pango.ALIGN_LEFT, font_size = 10):
         super(Paragraph,self).__init__()
-        if font:
-            self.font = font
-        else:
-            self.font = "Sans"    
-        if font_size:
-            self.font_size = font_size
-        else:
-            self.font_size = 10   
-        if text_align:
-            self.text_align = text_align
-        else:
-            self.text_align = pango.ALIGN_LEFT
+        self.font = font
+        self.font_size = font_size
+        self.font_size = 10
+        self.text_align = text_align
         if markup:
             self.is_markup = True
             self.text = markup
         else:
             self.text = text
         self.markup = markup
-   
-        
+
+
     def set_text(self,text):
         self.text = text
 
