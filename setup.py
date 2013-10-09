39e08a6e2a0d3520fb4481a94658a9bb25001ae9
Jishnu Mohan <jishnu7@gmail.com>
2013-10-09 23:44:13 +0530

    use Pillow instead of PIL.
    Since we already importing properly, we don't need any code change.

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     setup_requires = ['setuptools-git'],
     namespace_packages=['pypdflib'],
     include_package_data = True,
-    install_requires=['setuptools','PIL', 'hyphenation'],
+    install_requires=['setuptools','Pillow', 'hyphenation'],
     zip_safe = False,
     )
